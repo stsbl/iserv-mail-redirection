6362909b717bd1f7d3fbfbe0e2bcef0d7ca4207f
Felix Jacobi <felix.jacobi@iserv.eu>
2026-08-26 19:03:58 +0200

    Remove obsolete mail alias webpack build

--- modules/Stsbl/MailAliasBundle/Resources/webpack.config.js
+++
@@ -1,6 +0,0 @@
-// src/Stsbl/MailAliasBundle/Resources/webpack.config.js
-let merge = require('webpack-merge');
-let path = require('path');
-let baseConfig = require(path.join(process.env.WEBPACK_BASE_PATH, 'webpack.config.base.js'));
-
-module.exports = merge(baseConfig.get(__dirname), {});
