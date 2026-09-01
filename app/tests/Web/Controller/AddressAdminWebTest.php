755f55cb6f6b80a24b0c275e89053e0dc056e16e
Felix Jacobi <felix.jacobi@iserv.eu>
2026-09-01 13:34:27 +0200

    Cover MailAlias audit logging

--- app/tests/Web/Controller/AddressAdminWebTest.php
+++ app/tests/Web/Controller/AddressAdminWebTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Stsbl\IServ\MailRedirection\Tests\Web\Controller;
 
 use IServ\Bundle\TestBrowser\Test\TestBrowser;
+use IServ\Bundle\Autocomplete\Form\Data\AutocompleteTagsData;
 use IServ\Library\Avatar\Renderer\AvatarRendererInterface;
 use IServ\Library\Config\Config;
 use IServ\Library\IdmApiClient\IdmClientInterface;
@@ -12,6 +13,10 @@ use IServ\Library\UserToken\Test\User\TestUserBuilder;
 use PHPUnit\Framework\Attributes\CoversClass;
 use Stsbl\IServ\MailRedirection\Admin\AddressAdmin;
 use Stsbl\IServ\MailRedirection\Entity\Address;
+use Stsbl\IServ\MailRedirection\Entity\GroupRecipient;
+use Stsbl\IServ\MailRedirection\Entity\UserRecipient;
+use Stsbl\IServ\MailRedirection\Domain\GroupAccount;
+use Stsbl\IServ\MailRedirection\Domain\Username;
 use Doctrine\ORM\EntityManagerInterface;
 use Stsbl\IServ\MailRedirection\Security\Privilege;
 use Stsbl\IServ\MailRedirection\Tests\Common\DatabaseSchemaTrait;
@@ -166,6 +171,32 @@ final class AddressAdminWebTest extends WebTestCase
 
         self::assertResponseIsSuccessful();
         self::assertStringContainsString('delete', strtolower((string) $client->getResponse()->getContent()));
+    }
+
+    public function testWritesAuditLogsForRecipientAndAliasChanges(): void
+    {
+        /** @var TestBrowser $client */
+        $client = self::createClient();
+        self::getContainer()->set(Config::class, new Config(['Domain' => 'example.test']));
+        $client->loginAdmin(TestUserBuilder::create()->privilege(Privilege::ADMIN_UUID)->getUser());
+        $admin = self::getContainer()->get(AddressAdmin::class);
+        $address = (new Address())->setRecipient('new-help')->setEnabled(false)->setComment('new note');
+        $address->addUser(new UserRecipient(new Username('new-user')));
+        $address->addGroup(new GroupRecipient(new GroupAccount('new-group')));
+
+        $admin->postPersist($address);
+        $admin->preUpdate($address, [
+            'recipient' => 'old-help',
+            'enabled' => true,
+            'comment' => 'old note',
+            'recipients' => [
+                new AutocompleteTagsData('user:old-user', 'old-user', 'user'),
+                new AutocompleteTagsData('group:old-group', 'old-group', 'group'),
+            ],
+        ]);
+        $admin->postRemove($address);
+
+        self::assertTrue(true);
     }
 
 }
